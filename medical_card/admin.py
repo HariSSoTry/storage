fda0a3df82031c2d1ca76a95b9546f350942a065
HariSSoTry <ildar5yan@gmail.com>
2024-05-28 19:04:45 +0500

    основные модели с нуля

--- medical_card/admin.py
+++ medical_card/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-# admin.site.register(Patient)
-# admin.site.register(Folder)
-# admin.site.register(PatientDatabase)
-# admin.site.register(Document)
-
 admin.site.register(Symptom)
 admin.site.register(Disease)
 admin.site.register(Recommendation)
--- medical_card/models.py
+++ medical_card/models.py
@@ -2,53 +2,55 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# class Document(models.Model):
-#     title = models.CharField(max_length=200)
-#     content = models.TextField()
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-#
-#
-# class Folder(models.Model):
-#     name = models.CharField(max_length=200)
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-#     documents = models.ManyToManyField(Document)
-#
-#
-# class Patient(models.Model):
-#     name = models.CharField(max_length=200)
-#     birthdate = models.DateField()
-#     diagnosis = models.TextField()
-#     symptoms = models.TextField()
-#     allergies = models.TextField()
-#     medical_history = models.TextField()
-#     current_medication = models.TextField()
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-#
-#
-#
-# class PatientDatabase(models.Model):
-#     patients = models.ManyToManyField(Patient)
-#     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
 class Symptom(models.Model):
-    name = models.CharField(max_length=200)
-    # owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-class Recommendation(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    # author = models.CharField(max_length=200)
-    # date = models.DateField()
-    # owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
 
 
 class Disease(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     symptoms = models.ManyToManyField(Symptom)
-    treatment_recommendations = models.TextField()
-    # additional_documents = models.ManyToManyField(Document)
-    # owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def last_recommendation(self):
+        pass
+
+
+class Patient(models.Model):
+    name = models.CharField(max_length=200)
+    birthdate = models.DateField(blank=True, null=True)
+    # allergies = models.TextField()
+
+    def current_medication(self):   # todo добавить как метод для пациента
+        pass
+
+    def medical_history(self):
+        pass
+
+
+class Diagnosis(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
+    # date = models.DateField()
+
+
+class Recommendation(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    disease = models.ForeignKey(Disease, on_delete=models.CASCADE)
+    # author = models.CharField(max_length=200)
+    # date = models.DateField()
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    link = models.CharField(max_length=200)
+
+
+class Appointment(models.Model):
+    # date = models.DateField()
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    symptoms = models.ManyToManyField(Symptom)
+    diagnosis = models.ForeignKey(Diagnosis, on_delete=models.CASCADE)
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
