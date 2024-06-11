db6f5461981d2196ad8c7a540c3f7bda95b37443
HariSSoTry <ildar5yan@gmail.com>
2024-06-11 12:10:50 +0500

    Добавлена локальная БД, добавлены классы в admin.py

--- medical_card/admin.py
+++ medical_card/admin.py
@@ -4,3 +4,7 @@ from .models import *
 admin.site.register(Symptom)
 admin.site.register(Disease)
 admin.site.register(Recommendation)
+admin.site.register(Patient)
+admin.site.register(Diagnosis)
+admin.site.register(Document)
+admin.site.register(Appointment)
